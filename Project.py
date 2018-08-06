0fba50491a8ccc4acd6080853fbb8a0ef6044a28
TatianaVR <tsivkina@vrconcept.net>
2018-08-06 15:43:17 +0300

    Перемещение мыши

--- Project.py
+++ Project.py
@@ -3,3 +3,10 @@ import pyautogui
 pyautogui.size()
 (1920, 1080)
 width, height = pyautogui.size()
+
+#Перемещение мыши
+for i in range(10):
+      pyautogui.moveTo(100, 100, duration=0.25)
+      pyautogui.moveTo(200, 100, duration=0.25)
+      pyautogui.moveTo(200, 200, duration=0.25)
+      pyautogui.moveTo(100, 200, duration=0.25)
