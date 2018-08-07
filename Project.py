a16938f168621e6b79f8101c08024feb6a23de84
TatianaVR <tsivkina@vrconcept.net>
2018-08-07 11:41:39 +0300

    Получение и печать координат мыши, решение проблемы с отображением

--- Project.py
+++ Project.py
@@ -1,12 +1,8 @@
 
 import pyautogui
 print('Press Ctrl-F2 to quit.')
-try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-except KeyboardInterrupt:
-    print('\ nDone.')
-    print(positionStr, end='')
-    print('\b' * len(positionStr), end='', flush=True)
+x, y = pyautogui.position()
+positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+print('\ nDone.')
+print(positionStr)
 
