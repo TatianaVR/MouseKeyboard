0cd1ba596ffd6bd97c3e1786b4bd550f39ab315d
TatianaVR <tsivkina@vrconcept.net>
2018-08-07 10:51:12 +0300

    Получение и печать координат мыши

--- Project.py
+++ Project.py
@@ -7,5 +7,6 @@ try:
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
 except KeyboardInterrupt:
     print('\ nDone.')
-    print(positionStr)
+    print(positionStr, end='')
+    print('\b' * len(positionStr), end='', flush=True)
 
